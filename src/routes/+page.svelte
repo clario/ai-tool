b19605beec525408303833ed550b160ef73669d5
Eirik Måseidvåg <eirik.maaseidvaag@aritma.com>
2025-09-27 11:17:23 +0200

    feat: Add shadcn-svelte and button as test component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
+<script>
+	import Button from '$lib/components/ui/button/button.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 
 <a href="/secondPage">Gå til neste side</a>
+<Button>Click me</Button>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
